2cf0362ced22da55c0b3257a366f94a02b3da4fe
Jakub Vasinsky <jakub.vasinsky@akademiasovy.sk>
2019-10-22 10:36:29 +0200

    added random

--- MyArray.java
+++ MyArray.java
@@ -1,5 +1,7 @@
 package sk.itsovy.vasinsky.array;
 
+import java.util.Random;
+
 public class MyArray {
     public static void test1() {
         int[] arr = new int[7];
@@ -10,6 +12,20 @@ public class MyArray {
             System.out.print(arr[i]+" ");
         }
     }
+
+    public static void test2(){
+        Random rnd= new Random();
+        int[] arr = new int[7];
+        for (int i = 0; i <= 9; i++) {
+            arr[i] = rnd.nextInt(90)+10;
+        }
+        for (int i = 6; i >= 0; i--) {
+            System.out.print(arr[i]+" ");
+        }
+    }
+
+
+
 }
 
 
