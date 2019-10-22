a2546268b4e22842515b3cdcffe14ec25ba1b9c2
Jakub Vasinsky <jakub.vasinsky@akademiasovy.sk>
2019-10-22 11:11:57 +0200

    added test 2

--- Main.java
+++ Main.java
@@ -3,5 +3,6 @@ package sk.itsovy.vasinsky.array;
 public class Main {
     public static void main(String[] args){
 MyArray.test1();
+MyArray.test2();
     }
 }
--- MyArray.java
+++ MyArray.java
@@ -14,14 +14,21 @@ public class MyArray {
     }
 
     public static void test2(){
-        Random rnd= new Random();
-        int[] arr = new int[7];
-        for (int i = 0; i <= 9; i++) {
-            arr[i] = rnd.nextInt(90)+10;
-        }
-        for (int i = 6; i >= 0; i--) {
+        System.out.println();
+        int[] arr=new int [10];
+        Random rnd=new Random();
+        for(int i=0; i<=9; i++)
+            arr[i]=rnd.nextInt(90)+10;
+        int i = 0;
+        while(i < 10){
             System.out.print(arr[i]+" ");
+            i++;
         }
+        i=9;
+        do {
+            System.out.print(arr[i]+" ");
+            i--;
+        }while(i>=10);
     }
 
 
