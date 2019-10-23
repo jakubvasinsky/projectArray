afee305db1a7730d4198cc24e8dced29457265d8
Jakub Vasinsky <jakub.vasinsky@akademiasovy.sk>
2019-10-23 09:30:38 +0200

    added test3

--- Main.java
+++ Main.java
@@ -4,5 +4,7 @@ public class Main {
     public static void main(String[] args){
 MyArray.test1();
 MyArray.test2();
+MyArray.test3();
+
     }
 }
--- MyArray.java
+++ MyArray.java
@@ -9,30 +9,60 @@ public class MyArray {
             arr[i] = i + 1;
         }
         for (int i = 6; i >= 0; i--) {
-            System.out.print(arr[i]+" ");
+            System.out.print(arr[i] + " ");
         }
     }
 
-    public static void test2(){
+    public static void test2() {
         System.out.println();
-        int[] arr=new int [10];
-        Random rnd=new Random();
-        for(int i=0; i<=9; i++)
-            arr[i]=rnd.nextInt(90)+10;
+        int[] arr = new int[10];
+        Random rnd = new Random();
+        for (int i = 0; i <= 9; i++)
+            arr[i] = rnd.nextInt(90) + 10;
         int i = 0;
-        while(i < 10){
-            System.out.print(arr[i]+" ");
+        while (i < 10) {
+            System.out.print(arr[i] + " ");
             i++;
         }
-        i=9;
+        i = 9;
         do {
-            System.out.print(arr[i]+" ");
+            System.out.print(arr[i] + " ");
             i--;
-        }while(i>=10);
+        } while (i >= 10);
     }
 
+    public static void test3() {
+        System.out.println();
+        int[] arr = new int[12];
+        Random rnd = new Random();
+        for (int i = 0; i <= 11; i++) {
+            arr[i] = rnd.nextInt(10);
+        }
+        double average = 0;
+        for (int i = 0; i <= 11; i++) {
+            average = arr[i] + average;
+        }
+        average = average / 12;
+        System.out.println(average);
+        int max = arr[0], min = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (min > arr[i]) {
+                min = arr[i];
+                if (max < arr[i]) {
+                    max = arr[i];
+                }
+            }
+        }
+        System.out.println("Min: " + min);
+        System.out.println("Max: " + max);
+    }
 
 
 }
+
+
+
+
+
 
 
